580f4adb6cee3965259cfa60fb3f34a9efd4dff6
Ashley Bibizadeh <ashleybibizadeh@Ashleys-MacBook-Pro.local>
2019-01-26 15:34:36 +0000

    Header Component and Tests

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -4,10 +4,10 @@ import Logo from './../../assets/graphics/logo.png';
 
 const Header = (props) => {
     return (
-        <header>
+        <header data-test="headerComponent">
             <div className="wrap">
                 <div className="logo">
-                    <img src={Logo} alt="Logo" />
+                    <img data-test="logoIMG" src={Logo} alt="Logo" />
                 </div>
             </div>
         </header>
