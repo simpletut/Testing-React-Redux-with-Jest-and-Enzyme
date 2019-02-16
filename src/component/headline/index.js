f3fee1a097e1c2c4d5b9db41d9f1126082da24c1
Ashley Bibizadeh <ashleybibizadeh@Ashleys-MacBook-Pro.local>
2019-02-16 12:53:34 +0000

    Tweak to the headline component

--- src/component/headline/index.js
+++ src/component/headline/index.js
@@ -3,10 +3,6 @@ import PropTypes from 'prop-types';
 
 class Headline extends Component {
 
-    constructor(props){
-        super(props);
-    }
-
     render(){
 
         const { header, desc } = this.props;
